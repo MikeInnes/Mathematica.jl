3e475714f8e3f13188bcb971d15b19b8b707f202
Peng Guanwen <pg999w@outlook.com>
2020-02-07 18:37:29 +0800

    Finish more functions

--- src/operators.jl
+++ src/operators.jl
@@ -1,12 +1,40 @@
 (s::Symbol)(args...) = WExpr(buildexpr(s), buildexpr.(args))
 
-const WTypes = Union{WExpr, WSymbol, MathLink.WInteger, MathLink.WReal,
-                     Symbol}
-Base.:+(a::Union{WTypes, T}, b::Union{WTypes, T}) where T <: Number = plus(a, b)
-function plus(a, b)::WExpr
-    if isa(a, WExpr) && a.head == WSymbol("Plus")
-        WExpr(WSymbol("Plus"), (a.args..., buildexpr(b)))
+function buildwith(head, a, b)
+    if isa(a, WExpr) && a.head == WSymbol(head)
+        WExpr(WSymbol(head), (a.args..., buildexpr(b)))
     else
-        WExpr(WSymbol("Plus"), buildexpr.((a, b)))
+        WExpr(WSymbol(head), buildexpr.((a, b)))
     end
 end
+
+const WTypes = Union{WExpr, WSymbol, MathLink.WInteger, MathLink.WReal, Symbol}
+
+#
+# Arithematic operators
+#
+
+Base.:+(a::Union{WTypes, T}, b::Union{WTypes, T}) where T <: Number =
+    buildwith("Plus", a, b)
+
+Base.:-(a::WTypes) =
+    WSymbol("Times")(-1, buildexpr(a))
+
+Base.:-(a::Union{WTypes, T}, b::Union{WTypes, T}) where T <: Number =
+    a + (-b)
+
+Base.:*(a::Union{WTypes, T}, b::Union{WTypes, T}) where T <: Number =
+    buildwith("Times", a, b)
+
+Base.:/(a::Union{WTypes, T}, b::Union{WTypes, T}) where T <: Number =
+    a * WSymbol("Power")(buildexpr(b), -1)
+
+Base.:^(a::Union{WTypes, T}, b::Union{WTypes, T}) where T <: Number =
+    WSymbol("Power")(buildexpr(a), buildexpr(b))
+
+#
+# Functions
+#
+
+Base.:|>(x, f::Symbol) = buildexpr(f)(buildexpr(x))
+Base.inv(x::WTypes) = WSymbol("Inverse")(buildexpr(x))
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,40 @@ using Test
 using Mathematica: weval, buildexpr
 using MathLink: @W_str
 
-@testset "weval" begin
-    @testset "identity" begin
-        @test weval(1) == 1
-        # @test weval(:x) == :x
-    end
-end
-
 @testset "buildexpr" begin
     @test buildexpr(1) == 1
     @test buildexpr("s") == "s"
     @test buildexpr(:x) == W"x"
-    @test :x(1, :x) == W"x"(1, W"x")
-    @test :x + 1 == W"Plus"(W"x", 1)
-    @test :x + 2 + 3.2 == W"Plus"(W"x", 2, 3.2)
-    @test 1 + :Sin(:x) + 3.2 == W"Plus"(1, W"Sin"(W"x"), 3.2)
+end
+
+@testset "operators" begin
+    @testset "call" begin
+        @test :x(1, :x) == W"x"(1, W"x")
+    end
+    @testset "plus" begin
+        @test :x + 1 == W"Plus"(W"x", 1)
+        @test :x + 2 + 3.2 == W"Plus"(W"x", 2, 3.2)
+        @test 1 + :Sin(:x) + 3.2 == W"Plus"(1, W"Sin"(W"x"), 3.2)
+    end
+    @testset "minus" begin
+        @test -:x == W"Times"(-1, W"x")
+        @test 1 - :x == W"Plus"(1, W"Times"(-1, W"x"))
+        @test :x - 1 == W"Plus"(W"x", -1)
+        @test :x - :y - 1 == W"Plus"(W"x", W"Times"(-1, W"y"), -1)
+        @test 1 - (:x + 2) == W"Plus"(1, W"Times"(-1, W"Plus"(W"x", 2)))
+    end
+    @testset "times" begin
+        @test 2 * :x * :y == W"Times"(2, W"x", W"y")
+    end
+    @testset "divide" begin
+        @test 1 / :x == W"Times"(1, W"Power"(W"x", -1))
+        @test :x / 3 == W"Times"(W"x", W"Power"(3, -1))
+    end
+    @testset "power" begin
+        @test :a ^ :b ^ :c == W"Power"(W"a", W"Power"(W"b", W"c"))
+    end
+end
+
+@testset "functions" begin
+    @test (1 |> :Simplify) == W"Simplify"(1)
 end
